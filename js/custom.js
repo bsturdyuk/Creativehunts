d0c467c5387c92445c2db58e164c8164cdf9b13e
bsturdyuk <b.sturdyuk@gmail.com>
2017-12-05 16:00:12 +0700

    Our Works Page added, category filter added

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,35 @@ $(document).ready(function(){
   })
 
 
+  // BS Column Height Match
   $('.box').matchHeight();
+
+
+  //Category filter Feature
+  $(".filter-button").click(function(){
+       var value = $(this).attr('data-filter');
+
+       if(value == "all")
+       {
+           //$('.filter').removeClass('hidden');
+           $('.filter').show('1000');
+       }
+       else
+       {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+           $(".filter").not('.'+value).hide('3000');
+           $('.filter').filter('.'+value).show('3000');
+       }
+   });
+
+   if ($(".filter-button").removeClass("active")) {
+     $(this).removeClass("active");
+   }
+     $(this).addClass("active");
+
+
+
 });
 
 
