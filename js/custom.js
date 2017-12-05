65ee8b5087bb6a638ed2a5a8fafcda236c25d8a9
kenneth peter jones <kenjones620@yahoo.co.uk>
2017-12-05 22:25:39 +0000

    first step, fix indentation, remove comments, move all function calls into one document.ready() block

--- js/custom.js
+++ js/custom.js
@@ -1,104 +1,77 @@
 $(document).ready(function(){
 
   $('.menu-toggle').on('click', function(){
-   $('.full-menu').toggleClass('full-menu--open');
-  })
-
+    $('.full-menu').toggleClass('full-menu--open');
+  });
 
   // BS Column Height Match
   $('.box').matchHeight();
 
-
   //Category filter Feature
   $(".filter-button").click(function(){
-       var value = $(this).attr('data-filter');
+    var value = $(this).attr('data-filter');
 
-       if(value == "all")
-       {
-           //$('.filter').removeClass('hidden');
-           $('.filter').show('1000');
-       }
-       else
-       {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-           $(".filter").not('.'+value).hide('3000');
-           $('.filter').filter('.'+value).show('3000');
-       }
-   });
+    if(value == "all") {
+      $('.filter').show('1000');
+    } else {
+      $(".filter").not('.'+value).hide('3000');
+      $('.filter').filter('.'+value).show('3000');
+    }
 
-   if ($(".filter-button").removeClass("active")) {
-     $(this).removeClass("active");
-   }
-     $(this).addClass("active");
+    $(".filter-button").removeClass("active");
+    $(this).addClass("active");
+  });
 
+  scaleVideoContainer();
+  initBannerVideoSize('.video-container .poster img');
+  initBannerVideoSize('.video-container .filter');
+  initBannerVideoSize('.video-container video');
 
-
-});
-
-
-
-// Video bg
-//jQuery is required to run this code
-$( document ).ready(function() {
-
+  $(window).on('resize', function() {
     scaleVideoContainer();
-
-    initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
-    initBannerVideoSize('.video-container video');
-
-    $(window).on('resize', function() {
-        scaleVideoContainer();
-        scaleBannerVideoSize('.video-container .poster img');
-        scaleBannerVideoSize('.video-container .filter');
-        scaleBannerVideoSize('.video-container video');
-    });
-
+    scaleBannerVideoSize('.video-container .poster img');
+    scaleBannerVideoSize('.video-container .filter');
+    scaleBannerVideoSize('.video-container video');
+  });
 });
 
 function scaleVideoContainer() {
-
-    var height = $(window).height() + 5;
-    var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
-
+  var height = $(window).height() + 5;
+  var unitHeight = parseInt(height) + 'px';
+  $('.homepage-hero-module').css('height',unitHeight);
 }
 
 function initBannerVideoSize(element){
+  $(element).each(function(){
+    $(this).data('height', $(this).height());
+    $(this).data('width', $(this).width());
+  });
 
-    $(element).each(function(){
-        $(this).data('height', $(this).height());
-        $(this).data('width', $(this).width());
-    });
-
-    scaleBannerVideoSize(element);
-
+  scaleBannerVideoSize(element);
 }
 
 function scaleBannerVideoSize(element){
 
-    var windowWidth = $(window).width(),
-    windowHeight = $(window).height() + 5,
-    videoWidth,
-    videoHeight;
+  var windowWidth = $(window).width(),
+  windowHeight = $(window).height() + 5,
+  videoWidth,
+  videoHeight;
 
-    // console.log(windowHeight);
+  // console.log(windowHeight);
 
-    $(element).each(function(){
-        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+  $(element).each(function(el){
+    var videoAspectRatio = $(this).data('height')/$(this).data('width');
 
-        $(this).width(windowWidth);
+    $(this).width(windowWidth);
 
-        if(windowWidth < 1000){
-            videoHeight = windowHeight;
-            videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+    if(windowWidth < 1000){
+      videoHeight = windowHeight;
+      videoWidth = videoHeight / videoAspectRatio;
+      $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
 
-            $(this).width(videoWidth).height(videoHeight);
-        }
+      $(this).width(videoWidth).height(videoHeight);
+    }
 
-        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-    });
+    $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+  });
 }
